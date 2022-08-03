e21773a22ca2eddd8465ddd9adc3cfb577fd4858
Ichai Gutierrez <ichai.gutierrez@gmail.com>
2022-08-03 13:35:51 -0700

    Prueba de expresss
    Se cre una prueba con un ejemplo de post y get para visualizar el app en el navegador y en postman

--- op-api/routes/users.js
+++ op-api/routes/users.js
@@ -4,6 +4,17 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  res.send(req);
+});
+
+/*Ejemplo de post*/ 
+router.post('/hola', function (req, res) {
+  res.send('[POST]Saludos desde express equipo de ArjamexIt');
+});
+router.get('/hola', function (req, res) {
+  res.send('[GET]Saludos desde express equipo de ArjamexIt');
 });
 
 module.exports = router;
+
+
