fb49c0ea8c99ba0a1ca78a85e6977b684f8fb338
Ichai Gutierrez <ichai.gutierrez@gmail.com>
2022-08-03 14:01:30 -0700

    Sql Connection
    Se creo la coneccion de sql y se hizo una prueba de query

--- op-api/routes/users.js
+++ op-api/routes/users.js
@@ -7,12 +7,42 @@ router.get('/', function(req, res, next) {
   res.send(req);
 });
 
+
 /*Ejemplo de post*/ 
 router.post('/hola', function (req, res) {
   res.send('[POST]Saludos desde express equipo de ArjamexIt');
 });
 router.get('/hola', function (req, res) {
   res.send('[GET]Saludos desde express equipo de ArjamexIt');
+});
+
+
+//Sql conection
+var mysql      = require('mysql');
+var datos = [];
+var connection = mysql.createConnection({
+  host     : 'a2nlmysql11plsk.secureserver.net',
+  user     : 'arjamex',
+  password : 'Egf6@9t3',
+  port : '3306',
+  database: 'oposdb_'
+});
+
+connection.connect();
+connection.query('SELECT * FROM users', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0]);
+  datos.push(rows[0]);
+});
+
+
+
+
+connection.end();
+
+router.get('/conn', function (req, res) {
+  res.send(datos);
+ 
 });
 
 module.exports = router;
